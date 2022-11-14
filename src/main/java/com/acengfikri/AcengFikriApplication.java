16defde66919460ec358a864d2c0fb520c9bd10d
unknown <noverry.ambo@iconpln.co.id>
2022-11-14 22:16:40 +0700

    first commit

---
+++ src/main/java/com/acengfikri/AcengFikriApplication.java
@@ -0,0 +1,13 @@
+package com.acengfikri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcengFikriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcengFikriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acengfikri/AcengFikriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acengfikri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcengFikriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
